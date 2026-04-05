23657078c09692b16ec3d26d0f913bb820e076c2
Chazmander <chazgoulette@gmail.com>
2026-04-05 02:52:18 -0400

    add templates and routing for weather search functionality

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask,jsonify
+from flask import Flask,render_template,request
 from weather import get_weather
 
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Home Page"
+    return render_template("home.html")
 
-@app.route("/weather/<city>")
-def weather(city):
+@app.route("/weather")
+def weather():
+    city = request.args.get("city").title()
     data=get_weather(city)
     if data is None:
-        return "City not found"
-    return jsonify(data)
+        return "City not found", 404
+    return render_template("weather.html", data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
